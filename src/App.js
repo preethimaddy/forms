50deb11ad5020afa42d3892d22f9c2d6aa91bc54
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-11-15 13:05:19 +0530

    css styles update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+  const handleInput = () =>{
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1 className='text-3xl text-center my-4 py-3 px-2 mt-4 focus-outline-indigo-200'>
+     React Forms
+     <form className=' w=5/6 max-w-md mx-auto'>
+      <fieldset className='flex flex-col border py-1'>
+        <legend className='text-2xl font-semibold mb-2 text-gray-500'>About You</legend>
+        <label className='text-2xl text-left pl-2 font-semibold'>What's your name?</label>
+        <input className="border rounded text-lg 
+        leading-tight py-3 px-2 mt-4 mb-3 "id='firstName'
+        name='firstName'
+        placeholder='first name'
+        onChange={handleInput}>
+        </input>
+        <input className="border rounded text-lg 
+        leading-tight py-3 px-2 mt-4 focus:outline-indigo-200" id='lastName'
+        name='lastname'
+        placeholder='Last Name'
+        onChange={handleInput}>
+        </input>
+        <label className='text-2xl text-left pl-2 font-semibold'>What's your email?</label>
+             <input className="border rounded text-lg leading-tight py-3 px-2 mt-4 mb-3"id='email'
+        name='email'
+        placeholder='email@example.com'
+        onChange={handleInput}>
+        </input>
+        <label className='text-2xl text-left pl-2 font-semibold' >What's your date of birth?</label>
+        <input 
+        className="border rounded text-lg 
+        leading-tight py-3 px-2 mt-4 focus:outline-indigo-200"
+        id='dob'
+        name='dob'
+        type='date'
+        placeholder='pick your dob'
+        onChange={handleInput}></input>
+      </fieldset>
+     </form>
+   </h1>
+   
+   </>
   );
 }
 
